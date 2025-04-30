e4ffad3617ead0b77162970c45a977d9aae48079
alidowlat <14alida@gmail.com>
2025-04-30 22:08:14 +0330

    add assets configuration, add home urls path and handle 404 errors with custom view

--- main/urls.py
+++ main/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django.conf.urls
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import home.views
+from main import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+django.conf.urls.handler404 = home.views.error_404
