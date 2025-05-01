17a12b0624eecd2bcc2ec7b17879cefa93985d42
alidowlat <14alida@gmail.com>
2025-05-01 17:40:15 +0330

    small change on home.urls

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index_page'),
-    path('about-us/', views.about_us, name='about_us'),
+    path('about-us/', views.about_us, name='about_us_page'),
 ]
