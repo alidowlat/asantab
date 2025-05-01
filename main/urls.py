729989ada83354db259fc660ea35c0015b0bf672
alidowlat <14alida@gmail.com>
2025-05-01 17:39:54 +0330

    add accounts.urls to main.urls

--- main/urls.py
+++ main/urls.py
@@ -25,6 +25,7 @@ from main import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('auth/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
