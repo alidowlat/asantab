77d18e6e86197cb58ba67e5e0741f86f9454d0ff
alidowlat <14alida@gmail.com>
2025-05-02 22:48:43 +0330

    add models to django admin in admin.py.

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from services.models import Tag, Option, Service, Schedule, Profession, Media, Category
+
+admin.site.register(Category)
+admin.site.register(Media)
+admin.site.register(Option)
+admin.site.register(Profession)
+admin.site.register(Schedule)
+admin.site.register(Service)
+admin.site.register(Tag)
