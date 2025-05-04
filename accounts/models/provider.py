4549123c27269ff7c94b91d0577d11612afd3d03
alidowlat <14alida@gmail.com>
2025-05-04 13:59:13 +0330

    use image_compressor method in provider.py main model

--- accounts/models/provider.py
+++ accounts/models/provider.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.text import slugify
 from .user import User
 from core.media_path import get_image_upload_to
+from core.image_compressor import compress_and_convert_to_webp
 
 STATUS_CHOICES = (
     ('pending', 'در انتظار تایید'),
@@ -53,9 +54,21 @@ class Provider(models.Model):
     def __str__(self):
         return self.user.get_full_name() or self.user.phone_number
 
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.username)
+
+        if self.profile_image:
+            name_part = self.username
+            self.profile_image = compress_and_convert_to_webp(self.profile_image, name_part)
+
+        if self.national_card_image:
+            name_part = f"national_card_{self.username}"
+            self.national_card_image = compress_and_convert_to_webp(self.national_card_image, name_part)
+
         super().save(*args, **kwargs)
+
+
 
     class Meta:
         verbose_name = 'Provider'
