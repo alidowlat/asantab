959dd4cd5d4f09a5fcbd8f6f9bf99ce933d5dc69
alidowlat <14alida@gmail.com>
2025-05-04 13:44:38 +0330

    add services urls to main app

--- main/urls.py
+++ main/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('auth/', include('accounts.urls')),
+    path('services/', include('services.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
