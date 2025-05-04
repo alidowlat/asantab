a51bfab09f3138c5cd3a6e456520fb5fe02b876f
alidowlat <14alida@gmail.com>
2025-05-04 13:46:06 +0330

    changes on core/media_path.py file for better performance

--- core/media_path.py
+++ core/media_path.py
@@ -6,6 +6,5 @@ def get_image_upload_to(instance, filename):
     model_name = instance.__class__.__name__.lower()
     name_part = getattr(instance, 'username', None) or getattr(instance, 'slug', None) or model_name
     slug_name = slugify(name_part)
-    ext = filename.split('.')[-1]
-    filename = f"{slug_name}.{ext}"
+    filename = f"{slug_name}.webp"
     return os.path.join(model_name, slug_name, filename)
