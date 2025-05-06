e89dbd3c296c46e6de986c32d097e24d4e9247af
alidowlat <14alida@gmail.com>
2025-05-06 13:26:39 +0330

    add toggle_unique_status in urls/list.py

--- services/urls/list.py
+++ services/urls/list.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from services.views.list import ServiceListView
+from services.views.list import ServiceListView, toggle_unique_status
 
 urlpatterns = [
     path('', ServiceListView.as_view(), name='services_list_page'),
+    path('<int:pk>/toggle-unique/', toggle_unique_status, name='toggle_unique'),
 ]
