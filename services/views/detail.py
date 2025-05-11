3b9f8354d3c885ff6fe18c86a49ca1236340e95f
alidowlat <14alida@gmail.com>
2025-05-11 15:42:13 +0330

    handle min and max price in ServiceDetailView

--- services/views/detail.py
+++ services/views/detail.py
@@ -12,14 +12,13 @@ class ServiceDetailView(DetailView):
         loaded_service = self.object
 
         prices = list(loaded_service.options.filter(unit_price__isnull=False).values_list('unit_price', flat=True))
-        min_price = loaded_service.min_price = min(prices) if prices else None
-        max_price = loaded_service.max_price = max(prices) if prices else None
+        min_price = min(prices) if prices else None
+        max_price = max(prices) if prices else None
         context['min_price'] = min_price
         context['max_price'] = max_price
 
         related_services = Service.objects.filter(
             Q(profession_id=loaded_service.profession_id) |
-            Q(is_unique=True) |
             Q(tags__in=loaded_service.tags.all())
         ).exclude(id=loaded_service.id).distinct()
         context['related_services'] = related_services
