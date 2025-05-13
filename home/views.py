48a0dc3f3487e0b2c6730177a03cc159086eb3a8
alidowlat <14alida@gmail.com>
2025-05-13 14:46:59 +0330

    create context data for HomeView, include in index.html

--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
+from services.models import Service
 
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        request = self.request
+
+        featured_services = Service.objects.filter(is_unique=True, is_active=True, status='approved')[:8]
+        context['featured_services'] = featured_services
+
+        return context
 
 
 def site_header_component(request):
