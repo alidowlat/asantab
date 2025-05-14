2e0da4c7dd0c43576627d6d2d78d881c7b34129f
alidowlat <14alida@gmail.com>
2025-05-14 11:02:20 +0330

    create new model called social link and migration

--- config/admin.py
+++ config/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from config.models import SiteSetting, CategoryItem, MainCategory
+from config.models import SiteSetting, CategoryItem, MainCategory, SocialLink
 
 admin.site.register(SiteSetting)
 admin.site.register(MainCategory)
 admin.site.register(CategoryItem)
+admin.site.register(SocialLink)
