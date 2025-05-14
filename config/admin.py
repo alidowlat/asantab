349d1b83451b396d116ba4925c4c5f3f70c7eca5
alidowlat <14alida@gmail.com>
2025-05-14 11:05:00 +0330

    remove MainCategory from admin

--- config/admin.py
+++ config/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from config.models import SiteSetting, CategoryItem, MainCategory, FooterBox, FooterLink, SocialLink
+from config.models import SiteSetting, CategoryItem, FooterBox, FooterLink, SocialLink
 
 admin.site.register(SiteSetting)
 admin.site.register(FooterBox)
