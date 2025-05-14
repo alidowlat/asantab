74a0de9af9ced02c0a15680c5544165d27883669
alidowlat <14alida@gmail.com>
2025-05-14 10:50:10 +0330

    add site setting conetxt to footer and header view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
+from config.models import SiteSetting, FooterBox
 from services.models import Service
 
 
@@ -11,18 +12,33 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         request = self.request
 
-        featured_services = Service.objects.filter(is_unique=True, is_active=True, status='approved')[:8]
+        featured_services = Service.objects.filter(is_unique=True, is_active=True, status='approved').order_by('-id')[:5]
         context['featured_services'] = featured_services
+
+        newest_services = Service.objects.filter(is_active=True, status='approved').order_by('-id')[:10]
+        context['newest_services'] = newest_services
 
         return context
 
 
 def site_header_component(request):
-    return render(request, 'shared/header_comp.html')
+    site_settings = SiteSetting.objects.get(is_main=True)
+
+    context = {
+        'site_settings': site_settings,
+    }
+    return render(request, 'shared/header_comp.html', context)
 
 
 def site_footer_component(request):
-    return render(request, 'shared/footer_comp.html')
+    site_settings = SiteSetting.objects.get(is_main=True)
+    footer_boxes = FooterBox.objects.prefetch_related('footerlink_set').all()
+
+    context = {
+        'site_settings': site_settings,
+        'footer_boxes': footer_boxes,
+    }
+    return render(request, 'shared/footer_comp.html', context)
 
 
 def about_us(request):
