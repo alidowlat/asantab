2600e990e77443e65d45b688d9b7ca7ef6016871
alidowlat <14alida@gmail.com>
2025-05-14 10:49:18 +0330

    add footer box and link in admin

--- config/admin.py
+++ config/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
-from config.models import SiteSetting, CategoryItem, MainCategory
+from config.models import SiteSetting, CategoryItem, MainCategory, FooterBox, FooterLink
 
 admin.site.register(SiteSetting)
+admin.site.register(FooterBox)
+admin.site.register(FooterLink)
 admin.site.register(MainCategory)
 admin.site.register(CategoryItem)
