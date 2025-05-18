46e515f5e74c6fd373d0ede8a1477cfaaac9a94d
alidowlat <14alida@gmail.com>
2025-05-18 23:44:02 +0330

    create ServiceReview and ServiceReviewReaction models

---
+++ reviews/models/service_review.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from config.models import BaseReview, BaseReviewReaction
+
+
+class ServiceReview(BaseReview):
+    service = models.ForeignKey('services.Service', on_delete=models.CASCADE, related_name='reviews', verbose_name='خدمت')
+
+    class Meta:
+        verbose_name = 'Service Review'
+        verbose_name_plural = 'Service Reviews'
+        db_table = 'service_reviews'
+
+
+class ServiceReviewReaction(BaseReviewReaction):
+    review = models.ForeignKey(ServiceReview, on_delete=models.CASCADE, related_name='reactions', verbose_name='نظر')
+
+    class Meta:
+        unique_together = ('user', 'review')
+        verbose_name = 'Service Review Reaction'
+        verbose_name_plural = 'Service Review Reactions'
+        db_table = 'service_review_reactions'
+
+    def __str__(self):
+        return f"{self.reaction} - {self.review}"
