2d0fda589feb39601e3dd165ade0fdaf4b7e3ede
alidowlat <14alida@gmail.com>
2025-05-18 23:51:49 +0330

    create service_reviews context in ServiceDetailView

--- services/views/detail.py
+++ services/views/detail.py
@@ -1,5 +1,7 @@
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.views.generic import DetailView
+
+from reviews.models.service_review import ServiceReview
 from services.models import Service
 
 
@@ -21,7 +23,12 @@ class ServiceDetailView(DetailView):
             Q(profession__in=loaded_service.profession.all()) |
             Q(tags__in=loaded_service.tags.all())
         ).exclude(id=loaded_service.id).distinct()
-
         context['related_services'] = related_services
+
+        service_reviews = ServiceReview.objects.filter(service_id=loaded_service.id).annotate(
+            like_count=Count('reactions', filter=Q(reactions__reaction='like')),
+            dislike_count=Count('reactions', filter=Q(reactions__reaction='dislike'))
+        ).order_by('-created_at')
+        context['service_reviews'] = service_reviews
 
         return context
