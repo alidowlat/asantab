f6bd40dd47984a80354c2b4284bc55ff3b45820f
alidowlat <14alida@gmail.com>
2025-05-21 12:19:17 +0330

    add logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.phone_input_view, name='auth_page'),
     path('verify/', views.otp_verify_view, name='verify_page'),
     path('resend-otp/', resend_otp.resend_otp_view, name='resend_otp'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
