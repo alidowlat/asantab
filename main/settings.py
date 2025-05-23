9883c5342ea87a267b8de5a7221df1972f86035b
alidowlat <14alida@gmail.com>
2025-05-23 13:19:28 +0330

    delete useless comments on main project settings

--- main/settings.py
+++ main/settings.py
@@ -2,10 +2,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+3dsd!*40*i!&fp%)x&eh=ny0*q5y#am!a6r1=umkywo951-3v'
 
 DEBUG = True
