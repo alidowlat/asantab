cb0c2795ff79d525353f2a7e94b8b7e98f4ec37c
alidowlat <14alida@gmail.com>
2025-05-27 16:09:41 +0330

    create views and urls for handle forms

--- dashboard/urls/account.py
+++ dashboard/urls/account.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from dashboard.views.account import AccountView
+from dashboard.views import AccountView, UpdateEmailView, UpdateNameView, UpdatePhoneView, UpdateNationalIDView
 
 urlpatterns = [
     path('info/', AccountView.as_view(), name='account_info_page'),
+    path('update-email/', UpdateEmailView.as_view(), name='update_email'),
+    path('update-name/', UpdateNameView.as_view(), name='update_name'),
+    path('update-phone/', UpdatePhoneView.as_view(), name='update_phone'),
+    path('update-national-id/', UpdateNationalIDView.as_view(), name='update_national_id'),
 ]
--- dashboard/views/account.py
+++ dashboard/views/account.py
@@ -1,14 +1,81 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
+from django.views.generic import TemplateView
+from dashboard.forms import UpdateEmailForm, UpdateNameForm, UpdatePhoneForm, UpdateNationalIDForm
 
 
-class AccountView(LoginRequiredMixin, View):
+# class AccountView(LoginRequiredMixin, View):
+#     def get(self, request):
+#         return render(request, 'dashboard/account/main.html', {})
+
+
+class AccountView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/account/main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['email_form'] = UpdateEmailForm(instance=user)
+        context['name_form'] = UpdateNameForm(instance=user)
+        context['phone_form'] = UpdatePhoneForm(instance=user)
+        context['national_id_form'] = UpdateNationalIDForm(instance=user)
+        return context
+
+
+class UpdateEmailView(LoginRequiredMixin, View):
     def get(self, request):
-        context = {
-            'user': request.user,
-        }
-        return render(request, 'dashboard/account/main.html', context)
+        email_form = UpdateEmailForm(instance=request.user)
+        return render(request, 'dashboard/account/modal/email_modal.html', {'email_form': email_form})
 
     def post(self, request):
-        pass
+        form = UpdateEmailForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True, 'redirect_url': reverse('account_info_page')})
+
+        return JsonResponse({'success': False, 'errors': form.errors.get_json_data()})
+
+
+class UpdateNameView(LoginRequiredMixin, View):
+    def get(self, request):
+        name_form = UpdateNameForm(instance=request.user)
+        return render(request, 'dashboard/account/modal/name_modal.html', {'name_form': name_form})
+
+    def post(self, request):
+        form = UpdateNameForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True, 'redirect_url': reverse('account_info_page')})
+
+        return JsonResponse({'success': False, 'errors': form.errors.get_json_data()})
+
+
+class UpdatePhoneView(LoginRequiredMixin, View):
+    def get(self, request):
+        phone_form = UpdatePhoneForm(instance=request.user)
+        return render(request, 'dashboard/account/modal/phone_modal.html', {'phone_form': phone_form})
+
+    def post(self, request):
+        form = UpdatePhoneForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True, 'redirect_url': reverse('account_info_page')})
+
+        return JsonResponse({'success': False, 'errors': form.errors.get_json_data()})
+
+
+class UpdateNationalIDView(LoginRequiredMixin, View):
+    def get(self, request):
+        national_id_form = UpdateNationalIDForm(instance=request.user)
+        return render(request, 'dashboard/account/modal/national_id_modal.html', {'national_id_form': national_id_form})
+
+    def post(self, request):
+        form = UpdateNationalIDForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True, 'redirect_url': reverse('account_info_page')})
+
+        return JsonResponse({'success': False, 'errors': form.errors.get_json_data()})
