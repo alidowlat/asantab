d60da5fcd1e77bdf71ac5c61095848b38c4a1443
alidowlat <14alida@gmail.com>
2025-06-03 23:30:39 +0330

    and add Logout view to urls

--- accounts/urls/provider.py
+++ accounts/urls/provider.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views.provider import provider_phone_input_view, provider_otp_verify_view, provider_complete_info_view
+from accounts.views.provider import provider_phone_input_view, provider_otp_verify_view, provider_complete_info_view, LogoutView
 from accounts.services import resend_otp
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('verify/', provider_otp_verify_view, name='verify_page_provider'),
     path('complete-info/', provider_complete_info_view, name='complete_info_page_provider'),
     path('resend-otp/', resend_otp.resend_otp_view, name='resend_otp_provider'),
+    path('logout/', LogoutView.as_view(), name='logout_provider'),
 ]
