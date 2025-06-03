ddab41ba38c60155c788f4484cc794cd4c26bb99
alidowlat <14alida@gmail.com>
2025-06-03 23:31:07 +0330

    add user and provider to dashboard view

--- dashboard/views/profile.py
+++ dashboard/views/profile.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+
+from accounts.models import Provider
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -8,5 +11,11 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         request = self.request
+        user = request.user
+        if user.is_provider:
+            provider = get_object_or_404(Provider, user=user)
+            context['provider'] = provider
+
+        context['user'] = user
 
         return context
