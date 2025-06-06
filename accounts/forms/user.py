0c87ad9e5f534e71a6fe582f01800fe832afb5f4
alidowlat <14alida@gmail.com>
2025-06-06 22:54:58 +0330

    add PhoneNumberCleanMixin to PhoneForm

--- accounts/forms/user.py
+++ accounts/forms/user.py
@@ -1,31 +1,11 @@
 from django import forms
-
-from core.clean import OTPCleanMixin
-from core.convertors import fa_to_en_digits
+from core.clean import OTPCleanMixin, PhoneNumberCleanMixin
 
 
-class PhoneForm(forms.Form):
+class PhoneForm(PhoneNumberCleanMixin ,forms.Form):
     phone_number = forms.CharField(
         max_length=11,
     )
-
-    def clean_phone_number(self):
-        phone = fa_to_en_digits(self.cleaned_data['phone_number'].strip())
-        errors = []
-
-        if not phone.isdigit():
-            errors.append("شماره موبایل نامعتبر است.")
-
-        if not phone.startswith("09"):
-            errors.append("شماره موبایل میبایست با ۰۹ شروع شود.")
-
-        if len(phone) != 11:
-            errors.append("شماره موبایل میبایست دقیقا ۱۱ رقمی باشد.")
-
-        if errors:
-            raise forms.ValidationError(errors)
-
-        return phone
 
 
 class OTPForm(OTPCleanMixin, forms.Form):
