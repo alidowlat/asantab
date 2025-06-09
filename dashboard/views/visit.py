7cbb4af608624cafe2c6198e34e22195f5a415bb
alidowlat <14alida@gmail.com>
2025-06-09 22:14:47 +0330

    change Visit to ServiceVisit in service view

--- dashboard/views/visit.py
+++ dashboard/views/visit.py
@@ -2,19 +2,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView
-from services.models import Visit
+from services.models import ServiceVisit
 
 
 class VisitListView(LoginRequiredMixin, ListView):
     template_name = 'dashboard/visit/main.html'
     context_object_name = 'visit_list'
-    model = Visit
+    model = ServiceVisit
 
     def get_context_data(self, **kwargs):
         context = super(VisitListView, self).get_context_data(**kwargs)
         request = self.request
         context['visit_list'] = (
-            Visit.objects.filter(user=request.user).select_related('service').order_by('-created_at')
+            ServiceVisit.objects.filter(user=request.user).select_related('service').order_by('-created_at')
         )
 
         return context
@@ -22,5 +22,5 @@ class VisitListView(LoginRequiredMixin, ListView):
 
 @login_required
 def dashboard_visit_list_partial(request):
-    visit_list = Visit.objects.filter(user=request.user).select_related('service').order_by('-created_at')
+    visit_list = ServiceVisit.objects.filter(user=request.user).select_related('service').order_by('-created_at')
     return render(request, 'dashboard/visit/list.html', {'visit_list': visit_list})
