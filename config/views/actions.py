0243b767a22b84abe808e30ab7129ee9c09abc91
alidowlat <14alida@gmail.com>
2025-06-09 22:34:00 +0330

    change Visit model to ServiceVisit

--- config/views/actions.py
+++ config/views/actions.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
-from services.models import Favorite, Visit
+from services.models import Favorite, ServiceVisit
 
 
 def apply_filters(request, queryset):
@@ -73,7 +73,7 @@ def delete_all_favorites(request):
 def delete_visit(request):
     service_id = request.POST.get('service_id')
     if service_id:
-        Visit.objects.filter(user=request.user, service_id=service_id).delete()
+        ServiceVisit.objects.filter(user=request.user, service_id=service_id).delete()
         return JsonResponse({'status': 'ok'})
     return JsonResponse({'status': 'error', 'message': 'service_id not provided'}, status=400)
 
@@ -81,5 +81,5 @@ def delete_visit(request):
 @require_POST
 @login_required
 def delete_all_visits(request):
-    Visit.objects.filter(user=request.user).delete()
+    ServiceVisit.objects.filter(user=request.user).delete()
     return JsonResponse({'status': 'ok'})
