6e357d437d51b7a0378dba2cdff44e1b37b4efe4
alidowlat <14alida@gmail.com>
2025-06-10 16:12:24 +0330

    register PostTag to models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from blog.models import Post, BlogVisit, Category
+from blog.models import Post, BlogVisit, Category, PostTag
 
 admin.site.register(Post)
 admin.site.register(BlogVisit)
+admin.site.register(PostTag)
 admin.site.register(Category)
