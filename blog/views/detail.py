09f9c9384b33dba428ecf82c8db77eace05613bf
alidowlat <14alida@gmail.com>
2025-06-11 00:04:47 +0330

    better logic to handle view context

--- blog/views/detail.py
+++ blog/views/detail.py
@@ -14,29 +14,25 @@ class BlogDetailView(DetailView):
         loaded_post = self.object
         user = self.request.user if self.request.user.is_authenticated else None
 
-        related_posts = (
-            Post.objects
-            .filter(Q(category=loaded_post.category))
-            .exclude(id=loaded_post.id)
-            .distinct()
-        )
-        context['related_posts'] = related_posts
+        model_fields = [
+            ('categories', Category.objects
+             .filter(is_active=True)
+             .annotate(active_posts_count=Count('posts', filter=Q(posts__is_active=True)))
+             ),
+            ('most_viewed_posts', Post.objects
+             .filter(is_active=True)
+             .exclude(pk=loaded_post.pk)
+             .annotate(view_count=Count('visits'))
+             .order_by('-view_count')[:4]
+             ),
+            ('related_posts', Post.objects
+             .filter(Q(category=loaded_post.category))
+             .exclude(id=loaded_post.id).distinct()
+             ),
+        ]
 
-        most_viewed_posts = (
-            Post.objects
-            .filter(is_active=True)
-            .exclude(pk=loaded_post.pk)
-            .annotate(view_count=Count('visits'))
-            .order_by('-view_count')[:4]
-        )
-        context['most_viewed_posts'] = most_viewed_posts
-
-        categories = (
-            Category.objects
-            .filter(is_active=True)
-            .annotate(active_posts_count=Count('posts', filter=Q(posts__is_active=True)))
-        )
-        context['categories'] = categories
+        for field_name, queryset in model_fields:
+            context[field_name] = queryset
 
         create_visit_clean(
             user=self.request.user,
