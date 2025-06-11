374e02cea9ccb0fb9a12f0b2d36aa9395462f637
alidowlat <14alida@gmail.com>
2025-06-11 22:14:00 +0330

    add delete_all_searches to urls

--- config/urls/actions.py
+++ config/urls/actions.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from config.views import favorite_list_partial, delete_favorite, delete_all_favorites, favorite_count, delete_visit, delete_all_visits
+from config.views import favorite_list_partial, delete_favorite, delete_all_favorites, favorite_count, delete_visit, delete_all_visits, \
+    delete_all_searches
 
 urlpatterns = [
     path('favorites/partial/', favorite_list_partial, name='favorite_partial_list'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('favorites/delete/all/', delete_all_favorites, name='delete_all_favorites'),
     path('visits/delete/', delete_visit, name='delete_visit'),
     path('visits/delete/all/', delete_all_visits, name='delete_all_visits'),
+    path('search/delete/all/', delete_all_searches, name='delete_all_searches'),
 ]
