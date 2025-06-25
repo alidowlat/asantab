ea0eb0206a14e67febe0af3f06bf5630cdf44d0d
alidowlat <14alida@gmail.com>
2025-06-25 14:49:04 +0330

    add notif_list_partial to urls

--- config/urls/actions.py
+++ config/urls/actions.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from dashboard.views import dashboard_notifications_partial
 from config.views import favorite_list_partial, delete_favorite, delete_all_favorites, favorite_count, delete_visit, delete_all_visits, \
-    delete_all_searches, read_all_notifs, read_notif, unread_notifications_count
+    delete_all_searches, read_all_notifs, read_notif, unread_notifications_count, notif_list_partial
 
 urlpatterns = [
     path('favorites/partial/', favorite_list_partial, name='favorite_partial_list'),
@@ -12,6 +12,7 @@ urlpatterns = [
     path('visits/delete/all/', delete_all_visits, name='delete_all_visits'),
     path('search/delete/all/', delete_all_searches, name='delete_all_searches'),
     path('notifications/partial/', dashboard_notifications_partial, name='notifications_partial_list'),
+    path('notif-list/partial/', notif_list_partial, name='notif_list_partial'),
     path('notifications/read/', read_notif, name='read_notif'),
     path('notifications/read/all/', read_all_notifs, name='read_all_notifs'),
     path('notifications/unread-count/', unread_notifications_count, name='unread_notifications_count'),
