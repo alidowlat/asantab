42651ce013bae5c46555c32588b0fa50ca6b4851
alidowlat <14alida@gmail.com>
2025-06-26 14:59:33 +0330

    add user_notifs in DashboardView

--- dashboard/views/profile.py
+++ dashboard/views/profile.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from accounts.models import Provider
+from notifications.models import Notification
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -12,10 +13,13 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context = super(DashboardView, self).get_context_data(**kwargs)
         request = self.request
         user = request.user
+        user_notifs = Notification.objects.filter(user_id=user.id, is_read=False)
+
         if user.is_provider:
             provider = get_object_or_404(Provider, user=user)
             context['provider'] = provider
 
+        context['user_notifs'] = user_notifs
         context['user'] = user
 
         return context
