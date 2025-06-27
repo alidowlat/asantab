5d231c96e9aca3d3791727699678003f1d1716e4
alidowlat <14alida@gmail.com>
2025-06-27 19:42:28 +0330

    Reapply "create order models and register into admin"
    This reverts commit 9d99be8842089bb38c0ad726e8ac90780a8cb142.

---
+++ orders/models/order_item.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('orders.Order', on_delete=models.CASCADE, related_name='items', verbose_name='سفارش')
+    service = models.ForeignKey('services.Service', on_delete=models.CASCADE, verbose_name='خدمت')
+    option = models.ForeignKey('services.Option', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='آپشن انتخابی')
+    count = models.PositiveIntegerField(verbose_name='تعداد')
+    final_price = models.PositiveIntegerField(verbose_name='قیمت نهایی')
+
+    class Meta:
+        verbose_name = 'آیتم سفارش'
+        verbose_name_plural = 'آیتم‌های سفارش'
+        db_table = 'order_items'
