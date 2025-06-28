8773cd39c6e1859e1f931498b32485804fbda23d
alidowlat <14alida@gmail.com>
2025-06-28 23:33:37 +0330

    add discount_amount to OrderCalculator

--- orders/services/order_service.py
+++ orders/services/order_service.py
@@ -1,6 +1,4 @@
 class OrderCalculator:
-    """" using encapsulation """
-
     def __init__(self, order):
         self.order = order
 
@@ -10,5 +8,13 @@ class OrderCalculator:
     def total_discount(self):
         return sum(item.discount_amount or 0 for item in self.order.items.all())
 
-    def discounted_price(self):
-        return self.total_price() - self.total_discount()
+    def discount_amount(self):
+        if self.order.discount_code:
+            if self.order.discount_code.amount:
+                return self.order.discount_code.amount
+            if self.order.discount_code.percent:
+                return (self.total_price() * self.order.discount_code.percent) / 100
+        return 0
+
+    def final_price(self):
+        return self.total_price() - self.total_discount() - self.discount_amount()
