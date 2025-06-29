6fc5f2cbc51ab35396639cb198b35fcaa74f5d72
alidowlat <14alida@gmail.com>
2025-06-29 22:50:30 +0330

    add null=True for provider field in Order model

--- orders/models/order.py
+++ orders/models/order.py
@@ -9,18 +9,53 @@ STATUS_CHOICES = [
 
 
 class Order(models.Model):
-    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='orders', verbose_name='کاربر')
-    provider = models.ForeignKey('accounts.Provider', on_delete=models.CASCADE, related_name='received_orders',
-                                 verbose_name='ارائه دهنده')
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending', verbose_name='وضعیت سفارش')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ثبت')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
-    rejection_reason = models.TextField(null=True, blank=True, verbose_name='دلیل رد شدن')
-    is_paid = models.BooleanField(default=False, verbose_name='پرداخت شده؟')
-    discount_code = models.ForeignKey('discounts.DiscountCode', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='کد تخفیف')
+    user = models.ForeignKey(
+        'accounts.User',
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='کاربر'
+    )
+    provider = models.ForeignKey(
+        'accounts.Provider',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='received_orders',
+        verbose_name='ارائه دهنده'
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='pending',
+        verbose_name='وضعیت سفارش'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=
+        'تاریخ ثبت')
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=
+        'تاریخ بروزرسانی')
+    rejection_reason = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='دلیل رد شدن'
+    )
+    is_paid = models.BooleanField(
+        default=False,
+        verbose_name='پرداخت شده؟'
+    )
+    discount_code = models.ForeignKey(
+        'discounts.DiscountCode',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        verbose_name='کد تخفیف'
+    )
 
     def __str__(self):
-        return f'{self.is_paid} | {self.user} - {self.provider.username} --> {self.status}'
+        return f'{self.is_paid} | {self.user} - {self.provider} --> {self.status}'
 
     def save(self, *args, **kwargs):
         if self.rejection_reason and self.status != 'rejected':
