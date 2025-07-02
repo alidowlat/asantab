4422ae7d3f57e6a5985517f266edc2605ed46db5
alidowlat <14alida@gmail.com>
2025-07-02 23:08:50 +0330

    adding add_service_to_cart to urls

--- services/urls/detail.py
+++ services/urls/detail.py
@@ -1,9 +1,10 @@
-from services.views import toggle_reaction_service, toggle_favorite_service
-from services.views.detail import ServiceDetailView, add_service_review
+from services.views import toggle_reaction_service, toggle_favorite_service, ServiceDetailView, add_service_review
+from orders.views import add_service_to_cart
 from django.urls import path
 
 urlpatterns = [
     path('<slug:slug>/', ServiceDetailView.as_view(), name='service_detail'),
+    path('add-to-cart', add_service_to_cart, name='add_service_to_cart'),
     path('add-review', add_service_review, name='add_service_review'),
     path('toggle-reaction', toggle_reaction_service, name='toggle_reaction_service'),
     path('toggle-favorite', toggle_favorite_service, name='toggle_favorite_service'),
