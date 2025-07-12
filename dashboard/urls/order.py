872fc20da1bb95c674313288df161ed1c8dbf427
alidowlat <14alida@gmail.com>
2025-07-12 23:13:07 +0330

    implement order_detail

--- dashboard/urls/order.py
+++ dashboard/urls/order.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from dashboard.views import OrderListView
+from dashboard.views import OrderListView, OrderDetailView
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='orders_page'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
+
 ]
--- dashboard/views/order.py
+++ dashboard/views/order.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from orders.models import Order, OrderItem
 from orders.services import OrderCalculator
+from orders.models import STATUS_CHOICES
 
 
 class OrderListView(LoginRequiredMixin, ListView):
@@ -12,8 +13,10 @@ class OrderListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(OrderListView, self).get_context_data(**kwargs)
 
-        def get_orders(is_paid, select_fields=None):
+        def get_orders(is_paid, select_fields=None, status=None):
             queryset = Order.objects.filter(user=self.request.user, is_paid=is_paid)
+            if status:
+                queryset = queryset.filter(status=status)
             if select_fields:
                 queryset = queryset.prefetch_related(
                     Prefetch('items', queryset=OrderItem.objects.select_related(*select_fields))
@@ -22,12 +25,34 @@ class OrderListView(LoginRequiredMixin, ListView):
                 queryset = queryset.prefetch_related('items')
             return queryset
 
-
         cart_items = get_orders(False, ['service', 'option', 'schedule'])
         order_items = get_orders(True, ['service'])
 
         context['cart_items'] = cart_items
         context['order_items'] = order_items
+
+        for status_value, _ in STATUS_CHOICES:
+            context[f'order_{status_value}'] = get_orders(True, ['service'], status_value)
+
         context['final_price'] = sum(OrderCalculator(order).final_price() for order in order_items)
+
+        return context
+
+
+class OrderDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'dashboard/order/order_detail.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderDetailView, self).get_context_data(**kwargs)
+        loaded_order = Order.objects.filter(user=self.request.user, is_paid=True).prefetch_related(
+            Prefetch('items', queryset=OrderItem.objects.select_related('service', 'option', 'schedule'))
+        ).first()
+        order = Order.objects.filter(user=self.request.user, is_paid=True)
+        calc = OrderCalculator(loaded_order) if loaded_order else None
+
+        context['discount_amount'] = calc.discount_amount() if calc else 0
+        context['loaded_order'] = loaded_order
+        context['final_price'] = sum(OrderCalculator(order).final_price() for order in order)
 
         return context
