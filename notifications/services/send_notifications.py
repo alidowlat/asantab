ff79d6417cf8f3c38fa951e0d44eb2c2acd887b6
alidowlat <14alida@gmail.com>
2025-07-17 18:15:20 +0330

    create send_custom_email and welcome.html template

--- notifications/services/send_notifications.py
+++ notifications/services/send_notifications.py
@@ -1,4 +1,8 @@
+from django.template.loader import render_to_string
+from main.settings import EMAIL_HOST_USER
 from notifications.models import NotificationType, Notification
+from django.core.mail import EmailMultiAlternatives
+
 
 
 def notify_user(user, title, message, type_key, link=None):
@@ -10,3 +14,15 @@ def notify_user(user, title, message, type_key, link=None):
         link=link,
         notif_type=notif_type
     )
+
+def send_custom_email(subject, to_emails, template_name, context):
+    html_content = render_to_string(template_name, context)
+
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=html_content,
+        from_email=EMAIL_HOST_USER,
+        to=[to_emails] if isinstance(to_emails, str) else to_emails
+    )
+    email.content_subtype = "html"
+    email.send()
