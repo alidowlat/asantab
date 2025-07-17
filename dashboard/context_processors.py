7ca9dcbfd4be0c67b708d10c751bb8c1bb262f23
alidowlat <14alida@gmail.com>
2025-07-17 18:13:16 +0330

    better exception

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,9 +1,14 @@
+from django.shortcuts import get_object_or_404
 from accounts.models import Provider
 from accounts.services import calculate_profile_completion
 
 
+
 def profile_completion(request):
-    provider = Provider.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_provider:
+        provider = get_object_or_404(Provider, user=request.user)
+    else:
+        provider = None
 
     if provider:
         completion_percent = calculate_profile_completion(provider)
