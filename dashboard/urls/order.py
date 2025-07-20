8480e682a308d7de6712282e40fc627ceec49f7f
alidowlat <14alida@gmail.com>
2025-07-20 13:03:57 +0330

    implement providers received orders

--- dashboard/urls/order.py
+++ dashboard/urls/order.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from dashboard.views import OrderListView, OrderDetailView
+from dashboard.views import *
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='orders_page'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
-
+    path('received-orders/', ProviderOrderListView.as_view(), name='received_orders_page'),
+    path('received-orders/<int:pk>/', ProviderOrderDetailView.as_view(), name='received_order_detail'),
 ]
--- dashboard/views/order.py
+++ dashboard/views/order.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
+from accounts.models import Provider
 from orders.models import Order, OrderItem
 from orders.services import OrderCalculator
 from orders.models import STATUS_CHOICES
@@ -43,16 +47,78 @@ class OrderDetailView(LoginRequiredMixin, DetailView):
     template_name = 'dashboard/order/order_detail.html'
     model = Order
 
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user, is_paid=True)
+
     def get_context_data(self, **kwargs):
-        context = super(OrderDetailView, self).get_context_data(**kwargs)
-        loaded_order = Order.objects.filter(user=self.request.user, is_paid=True).prefetch_related(
-            Prefetch('items', queryset=OrderItem.objects.select_related('service', 'option', 'schedule'))
-        ).first()
-        order = Order.objects.filter(user=self.request.user, is_paid=True)
-        calc = OrderCalculator(loaded_order) if loaded_order else None
+        context = super().get_context_data(**kwargs)
+        order = self.object
+        calc = OrderCalculator(order)
 
-        context['discount_amount'] = calc.discount_amount() if calc else 0
-        context['loaded_order'] = loaded_order
-        context['final_price'] = sum(OrderCalculator(order).final_price() for order in order)
+        context['discount_amount'] = calc.discount_amount()
+        context['loaded_order'] = order
+        context['final_price'] = calc.final_price()
+        return context
 
+
+class ProviderOrderListView(LoginRequiredMixin, ListView):
+    template_name = 'dashboard/order/provider/main.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(ProviderOrderListView, self).get_context_data(**kwargs)
+
+        def get_orders(is_paid, select_fields=None, status=None):
+            provider = get_object_or_404(Provider, user=self.request.user)
+            queryset = Order.objects.filter(provider=provider, is_paid=is_paid)
+            if status:
+                queryset = queryset.filter(status=status)
+            if select_fields:
+                queryset = queryset.prefetch_related(
+                    Prefetch('items', queryset=OrderItem.objects.select_related(*select_fields))
+                )
+            else:
+                queryset = queryset.prefetch_related('items')
+            return queryset
+
+        order_items = get_orders(True, ['service'])
+
+        context['order_items'] = order_items
+
+        for status_value, _ in STATUS_CHOICES:
+            context[f'order_{status_value}'] = get_orders(True, ['service'], status_value)
+
+        context['final_price'] = sum(OrderCalculator(order).final_price() for order in order_items)
+
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        provider = get_object_or_404(Provider, user=user)
+
+        if not user.is_provider or not provider:
+            return HttpResponseForbidden("شما دسترسی به این بخش ندارید.")
+
+        if not provider.is_profile_complete:
+            messages.warning(request, "برای مشاهده سفارش‌ها، لطفاً اطلاعات حساب خود را کامل کنید.")
+            return redirect('dashboard_page')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ProviderOrderDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'dashboard/order/provider/order_detail.html'
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(provider_id=self.request.user.id, is_paid=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order = self.object
+        calc = OrderCalculator(order)
+
+        context['discount_amount'] = calc.discount_amount()
+        context['loaded_order'] = order
+        context['final_price'] = calc.final_price()
         return context
