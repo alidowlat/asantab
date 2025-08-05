75e5feef3f4a1d5306341aa22efdc2af460a8f7d
alidowlat <14alida@gmail.com>
2025-08-05 16:15:40 +0330

    create url for  load_cities view

--- dashboard/urls/service.py
+++ dashboard/urls/service.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from dashboard.views import ProviderServiceList, ProviderServiceCreate, ProviderServiceEdit, dashboard_services_partial, \
-    delete_service, add_option, delete_option, add_schedule, delete_schedule
+    delete_service, add_option, delete_option, add_schedule, delete_schedule, load_cities
 
 urlpatterns = [
     path('services/', ProviderServiceList.as_view(), name='provider_service_list'),
     path('services/create/', ProviderServiceCreate.as_view(), name='provider_service_create'),
     path('services/edit/<slug>/', ProviderServiceEdit.as_view(), name='provider_service_edit'),
+    path('services/load-cities/', load_cities, name='load_cities'),
     path('services/partial/', dashboard_services_partial, name='provider_services_partial_dashboard'),
     path('services/delete/', delete_service, name='delete_service'),
     path('services/option/add/', add_option, name='add_option'),
@@ -13,4 +14,3 @@ urlpatterns = [
     path('services/schedule/add/', add_schedule, name='add_schedule'),
     path('services/schedule/delete/<int:pk>', delete_schedule, name='delete_schedule'),
 ]
-
