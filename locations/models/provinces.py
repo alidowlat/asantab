6b7d0b969860c902c8d66e383909ee211d9b5468
alidowlat <14alida@gmail.com>
2025-08-17 20:10:16 +0330

    change the __str__

--- locations/models/provinces.py
+++ locations/models/provinces.py
@@ -6,7 +6,7 @@ class Province(models.Model):
     name_en = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name='نام استان به انگلیسی')
 
     def __str__(self):
-        return f'{self.name_fa} - {self.name_en}'
+        return f'{self.name_fa}'
 
     class Meta:
         verbose_name = 'Province'
