e62c1bedab53a478d46fcf0307031da9249a1b1e
alidowlat <14alida@gmail.com>
2025-08-19 23:24:38 +0330

    register models into admin

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from wallet.models import Wallet, WalletTransaction
+
+admin.site.register(Wallet)
+admin.site.register(WalletTransaction)
