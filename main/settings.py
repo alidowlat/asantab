2db7ed057921d62b477ede7f73a3df2a36fb8ba0
alidowlat <14alida@gmail.com>
2025-08-23 15:36:04 +0330

    change ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-+3dsd!*40*i!&fp%)x&eh=ny0*q5y#am!a6r1=umkywo951-3v
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.7', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.7', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = []
 
