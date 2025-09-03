974fd666cdec35637f17964bbfcea7a74187dc12
alidowlat <14alida@gmail.com>
2025-09-03 22:23:05 +0330

    register PaymentGateway - PaymentTransaction to admin

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from wallet.models import Wallet, WalletTransaction, WithdrawalRequest, DepositRequest, SiteWallet
+from wallet.models import Wallet, WalletTransaction, WithdrawalRequest, DepositRequest, SiteWallet, PaymentGateway, PaymentTransaction
 
 admin.site.register(Wallet)
 admin.site.register(SiteWallet)
 admin.site.register(WalletTransaction)
 admin.site.register(WithdrawalRequest)
+admin.site.register(PaymentGateway)
+admin.site.register(PaymentTransaction)
 admin.site.register(DepositRequest)
