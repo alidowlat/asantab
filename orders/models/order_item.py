3997c53884e3603cfbf6bb1f5fd6af4e72220d3d
alidowlat <14alida@gmail.com>
2025-09-11 22:49:31 +0330

    add vendor_order field to OrderItem with FK

--- orders/models/order_item.py
+++ orders/models/order_item.py
@@ -3,6 +3,12 @@ from django.db import models
 
 class OrderItem(models.Model):
     order = models.ForeignKey('orders.Order', on_delete=models.CASCADE, related_name='items', verbose_name='سفارش')
+    vendor_order = models.ForeignKey(
+        'VendorOrder',
+        on_delete=models.CASCADE,
+        related_name='items',
+        null=True, blank=True
+    )
     service = models.ForeignKey('services.Service', on_delete=models.CASCADE, verbose_name='خدمت')
     option = models.ForeignKey('services.Option', on_delete=models.PROTECT, null=True, blank=True, verbose_name='آپشن انتخابی')
     schedule = models.ForeignKey('services.Schedule', on_delete=models.PROTECT, null=True, blank=True, verbose_name='تاریخ رزرو')
