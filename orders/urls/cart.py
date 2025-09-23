d09b548a9d6e52f5224f7df1aad66e8fcbd2fa57
alidowlat <14alida@gmail.com>
2025-09-23 16:10:30 +0330

    add new path to cart urls

--- orders/urls/cart.py
+++ orders/urls/cart.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from orders.views import user_cart, update_cart_data, change_cart_item_count, remove_cart_item, clear_cart_items, order_checkout
+from orders.views import user_cart, update_cart_data, change_cart_item_count, remove_cart_item, clear_cart_items, order_checkout, \
+    cart_cleanup_view
 
 urlpatterns = [
     path('cart', user_cart, name='user_cart_page'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('change-item', change_cart_item_count, name='change_cart_item_count'),
     path('remove-item', remove_cart_item, name='remove_cart_item'),
     path('clear-cart', clear_cart_items, name='clear_cart_items'),
+    path('cleanup-cart', cart_cleanup_view, name='cart_cleanup_view'),
 ]
