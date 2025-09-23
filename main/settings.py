142275bfe892ada230719d36b0c61517c884bbf1
alidowlat <14alida@gmail.com>
2025-09-23 19:13:24 +0330

    update settings.py

--- main/settings.py
+++ main/settings.py
@@ -122,6 +122,8 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = Path("/home/asantabc/public_html/asantab/media")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
