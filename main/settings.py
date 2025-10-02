240cd9752fe3cfd27a14142da6ec48c174016260
alidowlat <14alida@gmail.com>
2025-10-02 15:44:32 +0330

    add new context_processor

--- main/settings.py
+++ main/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'dashboard.context_processors.profile_completion',
+                'config.context_processors.user_role',
             ],
         },
     },
