95a81876be70ca0c130244df575901ad5afe55b5
alidowlat <14alida@gmail.com>
2025-10-02 15:46:41 +0330

    add new mixin and func decorator

--- core/mixins.py
+++ core/mixins.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 
 
@@ -19,4 +19,21 @@ def provider_active_required(view_func):
 class ActiveProviderRequiredMixin:
     @method_decorator(provider_active_required)
     def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+def expert_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated or not getattr(request.user, "support_agent", False):
+            return render(request, "home/404.html", status=404)
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
+
+
+class ExpertRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or not getattr(request.user, "support_agent", False):
+            return render(request, "home/404.html", status=404)
         return super().dispatch(request, *args, **kwargs)
