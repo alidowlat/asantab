3c2c1fff83894effd3de2706b531353dab7a727d
alidowlat <14alida@gmail.com>
2025-10-02 15:44:16 +0330

    add 404 url

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from home.views import HomeView, about_us
+from home.views import HomeView, about_us, error_404
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index_page'),
     path('about-us/', about_us, name='about_us_page'),
+    path('not-found/', error_404, name='error_404'),
 ]
